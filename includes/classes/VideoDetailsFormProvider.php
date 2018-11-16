8cc1257ba887487860df53f9f460cc8d0bd7c08c
Nikola <milje18@gmail.com>
2018-11-16 12:44:30 +0100

    PDO connection and upload form finished

--- includes/classes/VideoDetailsFormProvider.php
+++ includes/classes/VideoDetailsFormProvider.php
@@ -1,25 +1,43 @@
 <?php 
 
  class VideoDetailsFormProvider {
-     
+     //prosledjujemo promenljivu con iz config.php i pravimo konstruktor da bi mogli da je koristimo u ovoj klasi
+     // zatim u upload.php u instanci objekta VideoDetailsFormProvider ubacujemo $con promenljivu iz pdo config fajla.
+     //zatim u metodi createCategoriesInput prosledjujemo $this->con jer bi u suprotnom trazio promenljivu samo unutar metode
+     //a ovako smo je stavili u konstruktor i mozemo da je koristimo u klasi
+        
+     private $con;
+
+    public function __construct($con){
+
+        $this->con = $con;
+    }
+
     public function createUploadForm(){
+
 
         $fileInput = $this->createFileInput();
         $titleInput = $this->createTitleInput();
         $descriptionInput = $this->createDescriptionInput();
+        $privacyInput = $this->createPrivacyInput();
+        $categoriesInput = $this->createCategoriesInput();
+        $uploadButton = $this->createUploadButton();
 
         return "<form action='processing.php' method='POST'>
                     $fileInput
                     $titleInput
                     $descriptionInput
+                    $privacyInput
+                    $categoriesInput
+                    $uploadButton
                 </form>";
     }
 
     private function createFileInput(){
         return "<div class='form-group'>
-            <label for='exampleFormControlFile1'>Your file</label>
-            <input type='file' class='form-control-file' id='exampleFormControlFile1' name='fileInput' required>
-        </div>";
+                    <label for='exampleFormControlFile1'>Your file</label>
+                    <input type='file' class='form-control-file' id='exampleFormControlFile1' name='fileInput' required>
+                </div>";
     }
 
     private function createTitleInput() {
@@ -34,6 +52,39 @@
                 </div>";
     }
 
+    private function createPrivacyInput(){
+        
+        return "<div class='form-group'>
+                    <select class='form-control' name='privacyInput' >
+                        <option value='0'>Private</option>
+                        <option value='1'>Public</option>
+                    </select>
+                </div>";
+    }
+
+    private function createCategoriesInput(){
+
+            $query = $this->con->prepare("SELECT * FROM categories");
+            $query->execute();
+
+            $html = "<div class='form-group'>
+            <select class='form-control' name='categoryInput'>";
+
+                while($row = $query->fetch(PDO::FETCH_ASSOC)){
+                    $id = $row["id"];
+                    $name = $row["name"];
+                    $html.= "<option value='$id'>$name</option>";
+                }
+
+            $html.="</select>
+            </div>";
+
+            return $html;
+    }
+
+    private function createUploadButton(){
+        return "<button type='submit' class='btn btn-primary' name='uploadButton' >Upload</button>";
+    }
 
  }
 
---
+++ includes/config.php
@@ -0,0 +1,16 @@
+<?php 
+ob_start(); //turns on output buffering
+
+
+date_default_timezone_set("Europe/Budapest");
+
+try{
+
+    $con = new PDO("mysql:dbname=VideoTube;host=localhost", "root", "" );
+    $con->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_WARNING);
+}
+catch(PDOException $e){
+    echo "Connection failed: " . $e->getMessage(); 
+}
+
+?>
